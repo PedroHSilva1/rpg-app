69b343500c305096fd8c52943b0ab1405af3bbe0
pedro <pedrobiollo7@gmail.com>
2025-01-08 11:01:27 -0300

    U

--- src/screens/Deities.js
+++ src/screens/Deities.js
@@ -7,10 +7,36 @@ export default function Deities() {
     <ScrollView contentContainerStyle={styles.container}>
       
       <Card
-        title="Dyah - Ceifadora do Norte"
+        title="Lumini, Deusa da Luz"
         image={{ uri: "https://via.placeholder.com/300" }}
-        description="Dyah é uma entidade poderosa que reina sobre o norte. Sua história está envolta em mistérios."
+        
+        description={` Lumini é aquela que traz a luz para o mundo dos mortais.\n\n Cada nascer do dia carrega um sorriso alegre e contagiante da Deusa.\n\n Além de trazer a luz ao mundo, ela também tem a função de iluminar o caminho dos perdidos, trazer felicidade ao mundo humano e combater as criaturas da noite que habitam no submundo.`}
       />
+
+      <Card
+        title="Endrius, Deus da Terra"
+        image={{ uri: "https://via.placeholder.com/300" }}
+        description={` Endrius é o Deus mais jovem da criação.\n\n Sua função é vital para o mundo, ele é a força da terra e a vida nos bosques, florestas e onde habita o verde.\n\n Protetor dos animais e o mais calmo de todos, sendo também um dos que apoiam a pacificidade, mas que não deixa atos de maldade impunes, ainda mais quando isso afeta o verde.`}
+      />
+
+      <Card
+        title="xxxxx, Deus da Água"
+        image={{ uri: "https://via.placeholder.com/300" }}
+        description={` A divindade das águas abençoa este mundo com a magnitude fluvial, fazendo o fluxo do mundo andar para frente.\n\n Ensinou as demais raças o conceito (??), sabedoria e conhecimento sobre a vida marítima.\n\n Onde a guerra dos nêmesis acontecia, ela teve grande participação curando os aliados feridos, restaurando o equilíbrio mental, e purificando as mentes confusas.`}
+      />
+
+      <Card
+        title="Ignascia, Deusa do Fogo"
+        image={{ uri: "https://via.placeholder.com/300" }}
+        description={` A Deusa criadora das chamas, a primeira Deusa-Guerreira a existir.\n\n Ensinou os humanos as artes da guerra, lhes deu o discernimento sob a criação de armas e lhes trouxe o calor nas noites frias.\n\n Aquela que trouxe esperança quando não havia mais nenhuma, seu calor envolveu o mundo trazendo a chama da paixão e a motivação para continuar a lutar por aquilo que todos acreditavam:\n\n "Ainda que eu morra em meio aos monstros e o obscuro tente me devorar Continuo queimando e assim trarei um novo amanhã..."`}
+      />
+
+      <Card
+        title="Zephyria, Deusa dos Ventos"
+        image={{ uri: "https://via.placeholder.com/300" }}
+        description={` Zephyria a força dos ventos e herdeira do trono de Zephyrus, o antigo deus Criador do Vento que se sacrificou em nome do mundo.\n\n Ela traz consigo a resiliência dos céus, pregando liberdade e trazendo justiça aqueles que escravizam ou tomam para si a liberdade do outro.`}
+      />
+      
     </ScrollView>
   );
 }
